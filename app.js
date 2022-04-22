9f770363c2440dd089a0a565aad52fab5c1bc7c1
robinhood1712 <ademolaadewoleo1@gmail.com>
2022-04-23 04:24:05 +0800

    project start

---
+++ app.js
@@ -0,0 +1,24 @@
+// const navItems = document.querySelectorAll('.nav-ul');
+// const hamburger = document.querySelector('.hamburger');
+
+// array.from(navItems)
+
+// console.log (navItems.length)
+// hamburger.addEventListener('click', function (){
+//     navItems.classList.toggle('.show')
+// })
+// navItems.forEach (item => {
+//     item.addEventListener('click', () => {
+//         item.classList.toggle('.checked');
+//     })
+// })
+
+// function toggle () {
+// const nav = document.querySelectorAll(".nav-ul")
+// nav.classList.toggle('checked');
+
+// }
+// // for (let i = 0; i < navItems.length)
+
+// console.log (navItems)
+
